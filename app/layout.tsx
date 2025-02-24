8ca5a390c949d42a0908099eec99b8cae11c3bd1
Sergio <speschiera@gmail.com>
2025-02-24 22:45:49 +0000

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default async function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
+          suppressHydrationWarning
           className={cn(
             "bg-background mx-auto min-h-screen w-full scroll-smooth antialiased",
             inter.className
